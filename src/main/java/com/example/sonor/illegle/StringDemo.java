39c11f2abd67d23387f368fa67e97a31fc3dc2be
stephen_xin <5871300@qq.com>
2021-01-28 10:04:20 +0800

    test  == & equals

---
+++ src/main/java/com/example/sonor/illegle/StringDemo.java
@@ -0,0 +1,15 @@
+package com.example.sonor.illegle;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "bcd";
+        if(a == b) {
+            System.out.println("a == b");
+        }
+        if(a.equals(b)) {
+            System.out.println("a equals b");
+        }
+    }
+}
